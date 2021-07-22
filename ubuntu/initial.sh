8df802c09599ed08d770172e88a08506ceb08f16
Certseeds <51754303+Certseeds@users.noreply.github.com>
2021-07-23 01:33:43 +0800

    reorder the initial.sh
    </subject>
    
    Branch: master
    
    <type>:
    - [x] Bug fix
    - [ ] Bug fix (Test)
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    - [ ] This change requires a documentation update
    
    <body>
    
    <footer>
    
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

--- ubuntu/initial.sh
+++ ubuntu/initial.sh
@@ -6,33 +6,12 @@ set -eoux pipefail
 # @Author: nanoseeds
 # @Date: 2020-02-14 12:03:47
  # @LastEditors: nanoseeds
- # @LastEditTime: 2021-07-21 23:37:38
+ # @LastEditTime: 2021-07-23 01:27:30
 ###
 USER_AGENT="Mozilla/5.0 (X11;U;Linux i686;en-US;rv:1.9.0.3) Geco/2008092416 Firefox/3.0.3"
 UBUNTU_VERSION="$(lsb_release -c | sed 's/Codename://g' | xargs)"
 finish() {
     echo "${0} ${1} finish"
-}
-main_microsoft() {
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >microsoft.gpg
-    sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-}
-main_vscode() {
-    main_microsoft
-    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-    sudo rm microsoft.gpg
-    ## Install
-    sudo apt update
-    sudo apt install code
-}
-main_msedge() {
-    ## Setup
-    main_microsoft
-    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
-    sudo rm microsoft.gpg
-    ## Install
-    sudo apt update
-    sudo apt install microsoft-edge-dev
 }
 main_0() {
     #sudo chmod 0777 /usr/bin/screen
@@ -59,6 +38,10 @@ main_newergcc() {
     GCC_VERSION=11
     sudo apt-get install gcc-"${GCC_VERSION}" g++-"${GCC_VERSION}"
     gcc-"${GCC_VERSION}" --version
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 1110
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 750
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-11 1110
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 750
 }
 main_cmake() {
     sudo apt install apt-transport-https ca-certificates gnupg software-properties-common wget
@@ -83,11 +66,6 @@ main_python3() {
     sudo chmod 0755 "${pip_file_name}"
     sudo pip3 install numpy
 }
-main_pdftotext() {
-    sudo apt install libpoppler-cpp-dev pkg-config
-    pip3 install pdftotext         # then can import pdftotext
-    sudo apt install poppler-utils # then can run `pdftotext`
-}
 main_jdk_mvn() {
     sudo apt install openjdk-11-jdk openjdk-8-jdk maven
     mkdir -p "/etc/maven"
@@ -98,35 +76,6 @@ main_jdk_mvn() {
     sudo update-alternatives --display java
     sudo update-alternatives --config java
     sudo update-alternatives --config javac
-}
-main_texlive() {
-    mkdir -p "${HOME}"/zsh_include
-    origin="$(pwd)"
-    mkdir -p ./texlive
-    mkdir -p /media/tex
-    sudo mount -t auto -o loop ./texlive.iso /media/tex
-    cd /media/tex
-    sudo ./install-tl
-    #! then should input  `I`
-    sudo umount /media/tex
-    sudo rm -r /media/tex
-    cd "${origin}"
-}
-main_git() {
-    cp ./.gitconfig "${HOME}"/.gitconfig
-    mkdir -p "${HOME}"/template
-}
-main_githubcli() {
-    sudo apt install software-properties-common
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-    sudo apt-add-repository https://cli.github.com/packages
-    sudo apt update
-    sudo apt install gh
-}
-main_graphcard() {
-    # fuck `graphics-drivers`, it make 440 shortcut for 450, and 430 shortcut for 440, make drivers can not use at all.
-    ls
-    # fuck nvidia, in driver 440.100 does not match 440.33, can you believe?
 }
 main_ohmyzsh() {
     # download oh-my-zsh
@@ -152,33 +101,6 @@ main_ohmyzsh() {
         sudo chmod 0755 "${HOME}"/.oh-my-zsh/plugins/zsh-autosuggestions
     }
 }
-main_intelmkl() {
-    origin="$(pwd)"
-    mkdir -p ./intelmkl
-    cd ./intelmkl
-    wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB \
-        --user-agent="${USER_AGENT}" \
-        --no-check-certificate
-    sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-    echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
-    # or `sudo add-apt-repository "deb https://apt.repos.intel.com/oneapi all main"`
-    sudo apt update
-    sudo apt install intel-oneapi-mkl intel-oneapi-mkl-devel
-    cd "${origin}"
-    rm -rf ./intelmkl
-}
-main_anaconda() {
-    # anaconda
-    ANACONDA="Anaconda3-2020.07-Linux-x86_64.sh"
-    proxychains4 wget -c https://repo.anaconda.com/archive/"${ANACONDA}" \
-        --user-agent="${USER_AGENT}" \
-        --no-check-certificate
-    sudo chmod 0755 ./"${ANACONDA}"
-    sudo ./"${ANACONDA}"
-    rm ./"${ANACONDA}"
-    # TODO press enter && yes now
-    # TODO source ~/.zshrc
-}
 main_miniconda() {
     mkdir -p "${HOME}"/zsh_include
     MINICONDA="Miniconda3-py39_4.9.2-Linux-x86_64.sh"
@@ -196,44 +118,42 @@ main_conda() {
     conda update --all
     #   conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=10.2 -c pytorch
 }
-main_condaconfig() {
-    # set origin of anaconda
-    conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free
-    conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge
-    conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/bioconda
-    conda config --set show_channel_urls yes
-    conda config --show_channel_urls yes
+main_pdftotext() {
+    sudo apt install libpoppler-cpp-dev pkg-config
+    pip3 install pdftotext         # then can import pdftotext
+    sudo apt install poppler-utils # then can run `pdftotext`
 }
-main_cuda() {
-    #! FIRST, install driver!!
-    # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-    sudo cp cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-    # wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
-    sudo apt install -y ./cuda/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
-    sudo apt install -y ./cuda/cuda-repo-ubuntu1804-10-2-local_10.2.1-1_amd64.deb
-    sudo apt install -y ./cuda/cuda-repo-ubuntu1804-10-2-local_10.2.2-1_amd64.deb
-    sudo apt install -y ./cuda/libcudnn8_8.1.1.33-1+cuda10.2_amd64.deb
-    sudo apt install -y ./cuda/libcudnn8-dev_8.1.1.33-1+cuda10.2_amd64.deb
-    sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
-    sudo apt update
-    sudo apt -y install cuda
-}
-main_sshd() {
-    sudo chown -R 1000:1000 "${HOME}"/.ssh/* # make sure all filex own by normal user
-    sudo chmod 0700 "${HOME}"/.ssh
-    sudo chmod 0600 "${HOME}"/.ssh/*
-    sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
-    # do not forget to ln a *.pub as authorized_keys as login pub key
-}
-main_LD_LIBRARY_PATH() {
+main_texlive() {
     mkdir -p "${HOME}"/zsh_include
+    origin="$(pwd)"
+    mkdir -p ./texlive
+    mkdir -p /media/tex
+    sudo mount -t auto -o loop ./texlive.iso /media/tex
+    cd /media/tex
+    sudo ./install-tl
+    #! then should input  `I`
+    sudo umount /media/tex
+    sudo rm -r /media/tex
+    cd "${origin}"
 }
-main_caffe_ssd() {
-    #! c***f is dead, do not use it anymore.
-    sudo apt install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler \
-        libopenblas-dev liblapack-dev libatlas-base-dev \
-        libgflags-dev libgoogle-glog-dev liblmdb-dev
-    sudo apt install --no-install-recommends libboost-all-dev
+main_githubcli() {
+    sudo apt install software-properties-common
+    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+    sudo apt-add-repository https://cli.github.com/packages
+    sudo apt update
+    sudo apt install gh
+}
+main_anaconda() {
+    # anaconda
+    ANACONDA="Anaconda3-2020.07-Linux-x86_64.sh"
+    proxychains4 wget -c https://repo.anaconda.com/archive/"${ANACONDA}" \
+        --user-agent="${USER_AGENT}" \
+        --no-check-certificate
+    sudo chmod 0755 ./"${ANACONDA}"
+    sudo ./"${ANACONDA}"
+    rm ./"${ANACONDA}"
+    # TODO press enter && yes now
+    # TODO source ~/.zshrc
 }
 
 main_6() {
@@ -266,59 +186,13 @@ main_shellcheck() {
     cd "${envi}"
 }
 main_opencv3() {
-    # install dependency of opencv
-    sudo add-apt-repository "deb http://mirrors.aliyun.com/ubuntu/ xenial-security main"
-    main_0
-    sudo apt install libgtk2.0-dev pkg-config libavcodec-dev \
-        libavformat-dev libswscale-dev python-dev python-numpy libtbb2 \
-        libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev \
-        libdc1394-22-dev -y
-    # download
-    {
-        cd "${HOME}"
-        proxychains4 wget https://codeload.github.com/opencv/opencv/tar.gz/3.4.10
-        tar -vxf opencv--3.4.10.tar.gz
-        mkdir -p build_dir
-        cd build_dir
-        cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
-        #make -j "$(cat /proc/cpuinfo | grep "processor" | wc -l)"
-        make -j "$(nproc)"
-        make install
-        # TODO delete last filess
-        # then, lib is in  /usr/local/include/opencv2
-    }
+    sudo apt install libopencv-dev
 }
 main_go() {
     # set go
-    envi=$(pwd)
-    cd "${HOME}"
-    GO_FILE_NAME="go1.15.linux-amd64.tar.gz"
-    pcf wget https://golang.org/dl/"${GO_FILE_NAME}"
-    sudo tar -xzf "${GO_FILE_NAME}" -C /usr/local/
-    rm "${GO_FILE_NAME}"
-    # add GOPATH for /etc/profile and "${HOME}"/.zshrc now
-    cd "${envi}"
-}
-main_vmware() {
-    #only for vmware
-    sudo apt install open-vm-tools -y
-    # sudo apt install open-vm-tools-dkms -y
-    sudo apt install open-vm-tools-desktop -y
-    {
-        proxychains4 git clone https://github.com/rasa/vmware-tools-patches.git
-        cd vmware-tools-patches
-        . ./setup.sh
-        ./download-tools.sh latest
-        ./untar-and-patch.sh
-        ./compile.sh
-    }
-    {
-        sudo modprobe vmhgfs #检测是否存在
-        lsmod | grep vmhgfs
-        sudo mkdir /mnt/hgfs　#不存在要用接下来两条挂载
-        sudo mount -t vmhgfs .host:/ /mnt/hgfs
-    }
-
+    sudo add-apt-repository ppa:longsleep/golang-backports
+    sudo apt update
+    sudo apt install golang-go
 }
 main_ryus() {
     #download python2 and ryus
@@ -326,7 +200,7 @@ main_ryus() {
     #`python2` names `python` in ubuntu1804 and elders.
 }
 main_nodejs() {
-    main_version_of_nodejs=15
+    main_version_of_nodejs=16
     curl -sL https://deb.nodesource.com/setup_"${main_version_of_nodejs}".x | sudo -E bash -
     sudo apt install -y nodejs
 }
@@ -347,6 +221,98 @@ function main_sshkeygen() {
 function main_gpgkeygen() {
     gpg --list-secret-keys --keyid-format LONG
 }
+
+main_sshd() {
+    sudo chown -R 1000:1000 "${HOME}"/.ssh/* # make sure all filex own by normal user
+    sudo chmod 0700 "${HOME}"/.ssh
+    sudo chmod 0600 "${HOME}"/.ssh/*
+    sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
+    # do not forget to ln a *.pub as authorized_keys as login pub key
+}
+
+main_microsoft() {
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >microsoft.gpg
+    sudo install -o root -g root -m 0644 microsoft.gpg /etc/apt/trusted.gpg.d/
+}
+main_vscode() {
+    main_microsoft
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo rm microsoft.gpg
+    ## Install
+    sudo apt update
+    sudo apt install code
+}
+main_msedge() {
+    ## Setup
+    main_microsoft
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+    sudo rm microsoft.gpg
+    ## Install
+    sudo apt update
+    sudo apt install microsoft-edge-dev
+}
+main_intelmkl() {
+    origin="$(pwd)"
+    mkdir -p ./intelmkl
+    cd ./intelmkl
+    wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB \
+        --user-agent="${USER_AGENT}" \
+        --no-check-certificate
+    sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+    echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+    # or `sudo add-apt-repository "deb https://apt.repos.intel.com/oneapi all main"`
+    sudo apt update
+    sudo apt install intel-oneapi-mkl intel-oneapi-mkl-devel
+    cd "${origin}"
+    rm -rf ./intelmkl
+}
+main_cuda() {
+    #! FIRST, install driver!!
+    # wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+    sudo cp cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+    # wget https://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+    sudo apt install -y ./cuda/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+    sudo apt install -y ./cuda/cuda-repo-ubuntu1804-10-2-local_10.2.1-1_amd64.deb
+    sudo apt install -y ./cuda/cuda-repo-ubuntu1804-10-2-local_10.2.2-1_amd64.deb
+    sudo apt install -y ./cuda/libcudnn8_8.1.1.33-1+cuda10.2_amd64.deb
+    sudo apt install -y ./cuda/libcudnn8-dev_8.1.1.33-1+cuda10.2_amd64.deb
+    sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
+    sudo apt update
+    sudo apt -y install cuda
+}
+main_caffe_ssd() {
+    #! c***f is dead, do not use it anymore.
+    sudo apt install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler \
+        libopenblas-dev liblapack-dev libatlas-base-dev \
+        libgflags-dev libgoogle-glog-dev liblmdb-dev
+    sudo apt install --no-install-recommends libboost-all-dev
+}
+main_vmware() {
+    #only for vmware
+    sudo apt install open-vm-tools -y
+    # sudo apt install open-vm-tools-dkms -y
+    sudo apt install open-vm-tools-desktop -y
+    {
+        proxychains4 git clone https://github.com/rasa/vmware-tools-patches.git
+        cd vmware-tools-patches
+        . ./setup.sh
+        ./download-tools.sh latest
+        ./untar-and-patch.sh
+        ./compile.sh
+    }
+    {
+        sudo modprobe vmhgfs #检测是否存在
+        lsmod | grep vmhgfs
+        sudo mkdir /mnt/hgfs　#不存在要用接下来两条挂载
+        sudo mount -t vmhgfs .host:/ /mnt/hgfs
+    }
+}
+main_graphcard() {
+    # fuck `graphics-drivers`, it make 440 shortcut for 450, and 430 shortcut for 440, make drivers can not use at all.
+    ls
+    # fuck nvidia, in driver 440.100 does not match 440.33, can you believe?
+}
+
 main_114514() {
     sudo apt autoremove
     sudo apt autoclean
